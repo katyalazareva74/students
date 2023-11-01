6df7449aa9c701618059ad06d410eb2891344ee1
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-11-01 19:25:27 +0300

    Add code

--- src/main/java/students/Main.java
+++ src/main/java/students/Main.java
@@ -1,4 +1,30 @@
 package students;
 
+import java.util.*;
+
 public class Main {
+    /**
+     * Метод  main формирует массив студентов, оставляет только студентов, у которых специализация "Информатика",
+     * оставляет только  пять студентов, оставляет студентов, у которых средний балл выше чем 4,5, сортирует
+     * выборку студентов по уменьшению среднего балла и выводит эту информацию в консоль.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Student st1 = new Student("Иван", "Информатика", List.of(new Double[]{4.0, 5.0, 5.0, 5.0, 5.0}));
+        Student st2 = new Student("Анна", "Математика", List.of(new Double[]{4.0, 4.0, 4.0, 4.0}));
+        Student st3 = new Student("Мария", "Информатика", List.of(new Double[]{5.0, 5.0, 5.0, 5.0, 4.0}));
+        Student st4 = new Student("Андрей", "Информатика", List.of(new Double[]{5.0, 5.0, 5.0, 5.0}));
+        Student st5 = new Student("Сергей", "Математика", List.of(new Double[]{5.0, 4.0, 5.0, 3.0}));
+        Student st6 = new Student("Светлана", "Информатика", List.of(new Double[]{4.0, 5.0, 5.0, 3.0}));
+        Student st7 = new Student("Григорий", "Информатика", List.of(new Double[]{5.0, 4.0, 5.0, 5.0}));
+        Student st8 = new Student("Константин", "Информатика", List.of(new Double[]{5.0, 5.0, 5.0, 5.0}));
+        List<Student> students = Arrays.asList(st1, st2, st3, st4, st5, st6, st7, st8);
+        students.stream()
+                .filter(n -> n.getSpeciality().equals("Информатика"))
+                .limit(5)
+                .filter(n -> n.getAverageGrade() > 4.5)
+                .sorted((a, b) -> Student.compare(a, b))
+                .forEach(System.out::println);
+    }
+
 }
--- src/main/java/students/Student.java
+++ src/main/java/students/Student.java
@@ -1,4 +1,77 @@
 package students;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Student {
+    private String name;
+    private String speciality;
+    private double averagegrade;
+    private List<Double> grades;
+
+    public Student(String name, String speciality, List<Double> grades) {
+        this.name = name;
+        this.speciality = speciality;
+        this.grades = grades;
+    }
+
+    /**
+     * Метод вычисляет средний балл студента
+     * @return - возвращается средний балл студента
+     */
+    public double getAverageGrade() {
+        double ag = this.grades.stream()
+                .collect(Collectors.averagingDouble(p->p));
+        setAveragegrade(ag);
+        return ag;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: " + this.name + "  Speciality: " + this.speciality + "  AverageGrade: " + this.averagegrade);
+    }
+
+    /**
+     * Метод сравнения двух студентов по величине среднего балла
+     * @param - студент a
+     * @param  - студент b
+     * @return - если метод возвращает 0, то средние баллы студентов одинаковые;
+     *         если метод возвращает отрицательное число, то средний балл студента a больше среднего балла студента b;
+     *         если метод возвращает положительное число, то средний балл студента b больше среднего балла студента a;
+     */
+    public static int compare(Student a, Student b) {
+        return Double.compare(b.getAveragegrade(), a.getAveragegrade());
+    }
+
+    public void setGrades(List<Double> grades) {
+        this.grades = grades;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Double> getGrades() {
+        return grades;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setAveragegrade(double averagegrade) {
+        this.averagegrade = averagegrade;
+    }
+
+    public double getAveragegrade() {
+        return averagegrade;
+    }
 }
